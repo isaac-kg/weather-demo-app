d7fba407319c7444db7484b63efb6e0382bd38ec
isaac-kg <isaackagiso7@gmail.com>
2023-12-20 19:02:34 +0200

    hide site

--- src/App.jsx
+++ src/App.jsx
@@ -30,42 +30,45 @@ function App() {
 
 
   return (
-    <div className="container">
-      <div className="weather">
-        <div className="weather-info">
-          <p className="title">🌡️ Weather APP</p>
-          <p className="sub-title">San Francisco Bay Area</p>
-          <p className="date">
-            {new Date().toLocaleDateString("en-GB", {
-              year: "numeric",
-              month: "long",
-              day: "numeric"
-            })}
-          </p>
-          <p style={{margin: 0, fontSize: "30px"}}>{currentWeather?.temperature}°F</p>
-          <p>{currentWeather?.shortForecast}</p>
-        </div>
-        <div className="weather-more">
-          <p>Percipitation: {currentWeather?.probabilityOfPrecipitation?.value || 0}%</p>
-          <p>Humidity: {currentWeather?.relativeHumidity?.value || 0}% </p>
-          <p>Wind Speed: {currentWeather?.windSpeed} </p> 
-        </div>
-      </div>
-    <div>
+    // <div className="container">
+    //   <div className="weather">
+    //     <div className="weather-info">
+    //       <p className="title">🌡️ Weather APP</p>
+    //       <p className="sub-title">San Francisco Bay Area</p>
+    //       <p className="date">
+    //         {new Date().toLocaleDateString("en-GB", {
+    //           year: "numeric",
+    //           month: "long",
+    //           day: "numeric"
+    //         })}
+    //       </p>
+    //       <p style={{margin: 0, fontSize: "30px"}}>{currentWeather?.temperature}°F</p>
+    //       <p>{currentWeather?.shortForecast}</p>
+    //     </div>
+    //     <div className="weather-more">
+    //       <p>Percipitation: {currentWeather?.probabilityOfPrecipitation?.value || 0}%</p>
+    //       <p>Humidity: {currentWeather?.relativeHumidity?.value || 0}% </p>
+    //       <p>Wind Speed: {currentWeather?.windSpeed} </p> 
+    //     </div>
+    //   </div>
+    // <div>
 
-    {
-      weather.map((weather, index) => {
-        return <div className="days-weather" key={index}>
-          <div>
-            <p><strong>{weather.name}</strong></p>
-            <p style={{margin: 0}}>{weather.shortForecast}</p>
-          </div>
-          <p >{weather.temperature}°F</p>
-        </div>
-      })
-    }
-      </div>
-    </div>
+    // {
+    //   weather.map((weather, index) => {
+    //     return <div className="days-weather" key={index}>
+    //       <div>
+    //         <p><strong>{weather.name}</strong></p>
+    //         <p style={{margin: 0}}>{weather.shortForecast}</p>
+    //       </div>
+    //       <p >{weather.temperature}°F</p>
+    //     </div>
+    //   })
+    // }
+    //   </div>
+    // </div>
+    <h3 style={{textAlign: 'center', marginTop: "20%"}}>
+      Nothing to see here....
+    </h3>
   )
 }
 
